d0183b78aed51019250cfb5c390b4d5b0b606c0a
iura <iura.gaitur@gmail.com>
2015-02-27 15:52:23 +0200

    Main structure project

---
+++ Lab_1_sm/src/com/app/lab/interfaces/IProcessingMethods.java
@@ -0,0 +1,10 @@
+package com.app.lab.interfaces;
+
+public interface IProcessingMethods {
+	
+	void rotate90Degree();
+	void flip();
+	void changeBrightness(int value);
+	void changeContrast(int value);
+	void save();
+}
